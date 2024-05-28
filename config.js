f8c708cf205c416e6550c0865ec538ff04c43a95
whyweexist <167321903+whyweexist@users.noreply.github.com>
2024-05-28 23:57:26 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Happy Birthday Dear",
+
+    background: "background.jpg",
+
+    min1: "min1.jpg",
+    min2: "min2.jpg",
+    min3: "max3.jpg",
+    min4: "min4.jpg",
+    min5: "min5.jpg",
+    min6: "min6.jpg",
+
+    max1: "max1.jpg",
+    max2: "max2.jpg",
+    max3: "max3.jpg",
+    max4: "max4.jpg",
+    max5: "max5.jpg",
+    max6: "max6.jpg",
+}
